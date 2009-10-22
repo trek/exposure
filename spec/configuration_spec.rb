1c30352b2549a2e4a2a32b7644e5c7e0e1655cf9
Trek Glowacki <pietrekg@umich.edu>
2009-10-22 11:06:17 -0400

    added specs for :except option on Controller.expose

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -43,42 +43,40 @@ describe "included actions" do
   end
 end
 
-# describe "excluded actions" do
-#   setup = lambda {
-#     class ShipTypesController < ActionController::Base
-#       expose_many(:ship_types, :except => IncludedActions)
-#     end 
-#   }
-#   setup.call
-#   
-#   ActionController::Routing::Routes.draw do |map| 
-#     map.resources :ship_types
-#   end
-#   
-#   before(:each) do
-#     @controller = ShipTypesController.new
-#     @request    = ActionController::TestRequest.new
-#     @response   = ActionController::TestResponse.new
-#   end
-#   
-#   after(:each) do
-#     Object.remove_class(ShipTypesController)
-#     setup.call
-#   end
-#   
-#   after(:all) do
-#     Object.remove_class(ShipTypesController)
-#   end
-#   
-#   IncludedActions.each do |action|
-#     it "should have action #{action}" do
-#       @controller.should respond_to(action)
-#     end
-#   end
-#   
-#   ExcludedActions.each do |action|
-#     it "should have not action #{action}" do
-#       @controller.should_not respond_to(action)
-#     end
-#   end
-# end
+describe "excluded actions" do
+  setup = lambda {
+    class ShipTypesController < ActionController::Base
+      expose_many(:ship_types, :except => ExcludedActions)
+    end 
+  }
+  setup.call
+  
+  ActionController::Routing::Routes.draw do |map| 
+    map.resources :ship_types
+  end
+  
+  Object.remove_class(ShipTypesController)
+  
+  before(:each) do
+    setup.call
+    @controller = ShipTypesController.new
+    @request    = ActionController::TestRequest.new
+    @response   = ActionController::TestResponse.new
+  end
+  
+  after(:each) do
+    Object.remove_class(ShipTypesController)
+  end
+  
+  IncludedActions.each do |action|
+    it "should have action #{action}" do
+      @controller.should respond_to(action)
+    end
+  end
+  
+  ExcludedActions.each do |action|
+    it "should have not action #{action}" do
+      @controller.should_not respond_to(action)
+    end
+  end
+end
